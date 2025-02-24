bd2dba816cb46104cbdbe85ec5ac720b3636dee6
Kenneth <kenny@kennethchilds.com>
2025-02-24 00:41:00 -0500

    separate physics file

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,6 @@
 const std = @import("std");
 const rl = @import("raylib");
-
-const Body = struct {
-    pos: rl.Vector2,
-    vel: rl.Vector2,
-    mass: f32,
-    radius: f32,
-    color: rl.Color,
-};
+const phys = @import("physics.zig");
 
 const Star = struct {
     pos: rl.Vector2,
@@ -15,108 +8,61 @@ const Star = struct {
 };
 
 pub fn main() !void {
-
-    // window properties
-    const screenWidth: i32 = 800;
-    const screenHeight: i32 = 600;
-
-    rl.initWindow(screenWidth, screenHeight, "Orbital Revolution");
-    defer rl.closeWindow();
-    rl.setTargetFPS(60);
-
     const num_stars: i32 = 1000;
     var stars: [num_stars]Star = undefined;
     var i: usize = 0;
     while (i < num_stars) : (i += 1) {
         stars[i] = Star{
             .pos = rl.Vector2{
-                .x = std.crypto.random.float(f32) * @as(f32, @floatFromInt(screenWidth)),
-                .y = std.crypto.random.float(f32) * @as(f32, @floatFromInt(screenHeight)),
+                .x = std.crypto.random.float(f32) * @as(f32, @floatFromInt(phys.init.screenWidth)),
+                .y = std.crypto.random.float(f32) * @as(f32, @floatFromInt(phys.init.screenHeight)),
             },
             .color = rl.Color.white,
         };
     }
 
-    // initial conditions of celestial bodies
-    const gravity: f32 = 6.67e-1; // Our gravitational constant
-    const central_mass: f32 = 100.0;
-    const radius: f32 = 100.0;
-    const orbit_radius: f32 = radius * 2; // Distance from center to orbiting body
+    rl.initWindow(phys.init.screenWidth, phys.init.screenHeight, "Orbital Revolution");
+    defer rl.closeWindow();
 
-    // Calculate the required velocity for circular orbit: v = sqrt(G * M / r)
-    const orbital_velocity = @sqrt((gravity * central_mass) / orbit_radius);
-
-    var bodies = [_]Body{
-        // body 1 (orbiting body)
-        Body{
-            .pos = rl.Vector2{
-                .x = screenWidth / 4,
-                .y = screenHeight / 2,
-            },
-            .vel = rl.Vector2{
-                .x = 0,
-                .y = orbital_velocity,
-            },
-            .mass = 1.0,
-            .color = rl.Color.dark_gray,
-            .radius = radius / 10,
-        },
-
-        // body 2 (central body)
-        Body{
-            .pos = rl.Vector2{ .x = screenWidth / 2, .y = screenHeight / 2 },
-            .vel = rl.Vector2{ .x = 0, .y = 0 },
-            .mass = central_mass,
-            .color = rl.Color.dark_blue,
-            .radius = radius,
-        },
-    };
+    rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.black);
-        rl.drawFPS(
-            10,
-            10,
-        );
+        rl.drawFPS(10, 10);
 
-        // Calculate forces between bodies[0] and bodies[1]
-        const dx = bodies[1].pos.x - bodies[0].pos.x;
-        const dy = bodies[1].pos.y - bodies[0].pos.y;
-        const distance = rl.math.vector2Distance(bodies[0].pos, bodies[1].pos);
-        const force_magnitude = (gravity * bodies[0].mass * bodies[1].mass) / (distance * distance);
+        // math
+        const deltaX = phys.math.dx(phys.planetConstants.moon.pos.x, phys.planetConstants.earth.pos.x);
+        const deltaY = phys.math.dy(phys.planetConstants.moon.pos.y, phys.planetConstants.earth.pos.y);
+        const dist = phys.math.distance(deltaX, deltaY);
 
-        // Calculate force components
-        const force_x = force_magnitude * dx / distance;
-        const force_y = force_magnitude * dy / distance;
+        const force = phys.math.forceMagnitude(phys.constants.gravity, phys.planetConstants.earth.mass, phys.planetConstants.moon.mass, dist);
 
-        const dt: f32 = 10.0;
+        const forceX = -force * deltaX / dist;
+        const forceY = -force * deltaY / dist;
 
-        // Calculate acceleration components
-        const ax = force_x / bodies[0].mass;
-        const ay = force_y / bodies[0].mass;
+        const dt: f32 = 1.0;
+        const ax = forceX / phys.planetConstants.moon.mass;
+        const ay = forceY / phys.planetConstants.moon.mass;
 
-        // Update velocity and position
-        bodies[0].vel = rl.Vector2{
-            .x = bodies[0].vel.x + ax * dt,
-            .y = bodies[0].vel.y + ay * dt,
+        // move bitch
+        phys.planetConstants.moon.vel = rl.Vector2{
+            .x = phys.planetConstants.moon.vel.x + ax * dt,
+            .y = phys.planetConstants.moon.vel.y + ay * dt,
         };
-        bodies[0].pos = rl.Vector2{
-            .x = bodies[0].pos.x + bodies[0].vel.x * dt,
-            .y = bodies[0].pos.y + bodies[0].vel.y * dt,
+        phys.planetConstants.moon.pos = rl.Vector2{
+            .x = phys.planetConstants.moon.pos.x + phys.planetConstants.moon.vel.x * dt,
+            .y = phys.planetConstants.moon.pos.y + phys.planetConstants.moon.vel.y * dt,
         };
 
         for (stars) |star| {
             rl.drawPixel(@intFromFloat(star.pos.x), @intFromFloat(star.pos.y), star.color);
         }
 
-        for (bodies) |body| {
-            // draw circles, layout for drawCircle() is (centerX: i32, centerY: i32, radius: f32, color: Color) so use @ for type coercion to convert from f32 to i32
-            rl.drawCircle(@intFromFloat(body.pos.x), @intFromFloat(body.pos.y), body.radius, body.color);
-        }
-
-        std.debug.print("Orbiting Body Position: x={d:.3}, y={d:.3}\n", .{ bodies[0].pos.x, bodies[0].pos.y });
+        rl.drawCircle(@intFromFloat(phys.planetConstants.earth.pos.x), @intFromFloat(phys.planetConstants.earth.pos.y), phys.planetConstants.earth.radius, phys.planetConstants.earth.color);
+        rl.drawCircle(@intFromFloat(phys.planetConstants.moon.pos.x), @intFromFloat(phys.planetConstants.moon.pos.y), phys.planetConstants.moon.radius, phys.planetConstants.moon.color);
+        std.debug.print("Orbiting Body Position: x={d:.3}, y={d:.3}\n", .{ phys.planetConstants.moon.pos.x, phys.planetConstants.moon.pos.y });
     }
 }
---
+++ src/physics.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+//width: i32, height: i32, title: [*:0]const u8
+
+pub const init = struct {
+    pub const screenWidth = 800;
+    pub const screenHeight = 600;
+};
+
+pub const constants = struct {
+    pub const massEarth: f32 = 1000.0;
+    pub const massMoon: f32 = 10.0;
+    pub const gravity: f32 = 6.67e-1;
+    pub const centralMass: f32 = 100.0;
+    pub const radius: f32 = 100.0;
+    pub const orbitRadius: f32 = radius * 2;
+};
+
+pub const body = struct {
+    pos: rl.Vector2,
+    vel: rl.Vector2,
+    mass: f32,
+    radius: f32,
+    color: rl.Color,
+};
+
+pub const planetConstants = struct {
+    pub var earth = body{
+        .pos = .{
+            .x = init.screenWidth / 2,
+            .y = init.screenHeight / 2,
+        },
+        .vel = .{
+            .x = 0.0,
+            .y = 0.0,
+        },
+        .mass = constants.massEarth,
+        .radius = constants.radius,
+        .color = rl.Color.dark_blue,
+    };
+
+    pub var moon = body{
+        .pos = .{
+            .x = init.screenWidth / 4,
+            .y = init.screenHeight / 2,
+        },
+        .vel = .{
+            .x = 0.0,
+            .y = math.orbitalVelocity(constants.gravity, constants.massEarth, constants.orbitRadius),
+        },
+        .mass = constants.massMoon,
+        .radius = constants.radius / 10,
+        .color = rl.Color.gray,
+    };
+};
+
+pub const math: type = struct {
+    pub fn orbitalVelocity(gravity: f32, centralMass: f32, orbitRadius: f32) f32 {
+        return @sqrt((gravity * centralMass) / orbitRadius);
+    }
+
+    pub fn dx(x_pos_1: f32, x_pos_2: f32) f32 {
+        return x_pos_1 - x_pos_2;
+    }
+
+    pub fn dy(y_pos_1: f32, y_pos_2: f32) f32 {
+        return y_pos_1 - y_pos_2;
+    }
+
+    pub fn distance(delta_x: f32, delta_y: f32) f32 {
+        return @sqrt(delta_x * delta_x + delta_y * delta_y);
+    }
+
+    pub fn forceMagnitude(gravity: f32, bodyMass1: f32, bodyMass2: f32, dist: f32) f32 {
+        return (gravity * bodyMass1 * bodyMass2) / (dist * dist);
+    }
+};
